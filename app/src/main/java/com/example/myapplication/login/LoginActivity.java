e40e440077cc19897086dacb835d36acf37c73d9
LAPTOP-7UFJLJQ3\Admin <xuanthanhvdk@gmail.com>
2023-06-30 21:48:30 +0700

    đk đẩy dữ liệu lên firebase

--- app/src/main/java/com/example/myapplication/login/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/login/LoginActivity.java
@@ -5,20 +5,36 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.myapplication.MainActivity;
 import com.example.myapplication.R;
+import com.example.myapplication.filmlist.Film;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class LoginActivity extends AppCompatActivity {
 
     TextView createnewAccount;
-    @SuppressLint("MissingInflatedId")
+    private Button btnLogin,btnSignup,login;
+    private UserList userList;
+    public static FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    @SuppressLint({"WrongViewCast", "MissingInflatedId"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_login_1);
         createnewAccount=findViewById(R.id.createNewAccount);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
@@ -29,6 +45,42 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
+        EditText loginvalue;
+        EditText passw;
+        loginvalue = findViewById(R.id.email);
+        passw = findViewById(R.id.password);
+        btnLogin=(Button)findViewById(R.id.login);
+
+
+        login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = loginvalue.getText().toString();
+                String pass = passw.getText().toString();
+
+                Users user = new Users(name,pass);
+
+                if (user.checkValidEmail()&&user.checkValidPassword()){
+                    if (user.checkValidUser()){
+                        Toast toast = Toast.makeText(getApplicationContext(),"Đăng nhập thành công !", Toast.LENGTH_SHORT);
+                        toast.show();
+                        Intent intent2 = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent2);
+                    }
+                    else {
+                        Toast toast = Toast.makeText(getApplicationContext(),"User not existed", Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+                }
+                else {
+                    Toast toast = Toast.makeText(getApplicationContext(),"Tài khoản hoặc mật khẩu không chính xác.", Toast.LENGTH_SHORT);
+                    toast.show();
+
+                }
+
+            }
+        });
+    }
     }
 
-}
--- app/src/main/java/com/example/myapplication/user/Login.java
+++ app/src/main/java/com/example/myapplication/user/Login.java
@@ -1,71 +1,70 @@
-    package com.example.myapplication.user;
-
-    import androidx.appcompat.app.AppCompatActivity;
-
-    import android.annotation.SuppressLint;
-    import android.content.Intent;
-    import android.os.Bundle;
-    import android.view.View;
-    import android.widget.Button;
-    import android.widget.EditText;
-    import android.widget.TextView;
-    import android.widget.Toast;
-
-    import com.example.myapplication.MainActivity;
-    import com.example.myapplication.R;
-
-    public class Login extends AppCompatActivity {
-        private Button btnLogin,btnSignup,login;
-        private UserList userList;
-        @SuppressLint({"WrongViewCast", "MissingInflatedId"})
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_login);
-
-
-            EditText loginvalue;
-            EditText passw;
-            loginvalue = findViewById(R.id.loginvalue);
-            passw = findViewById(R.id.pass);
-            btnLogin=(Button)findViewById(R.id.btnLogin);
-            btnSignup=(Button)findViewById(R.id.btnSignup);
-
-            btnSignup.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(Login.this,SignUp.class);
-                    startActivity(intent);
-                }
-            });
-
-            login = findViewById(R.id.login);
-            login.setOnClickListener(new View.OnClickListener() {
-                 @Override
-                 public void onClick(View v) {
-                        String name = loginvalue.getText().toString();
-                        String pass = passw.getText().toString();
-
-                     Users user = new Users(name,pass);
-                     if (user.checkValidEmail()&&user.checkValidPassword()){
-                         if (user.checkValidUser()){
-                             Toast toast = Toast.makeText(getApplicationContext(),"Đăng nhập thành công !", Toast.LENGTH_SHORT);
-                             toast.show();
-                             Intent intent2 = new Intent(Login.this, MainActivity.class);
-                             startActivity(intent2);
-                         }
-                         else {
-                             Toast toast = Toast.makeText(getApplicationContext(),"User not existed", Toast.LENGTH_SHORT);
-                             toast.show();
-                         }
-                     }
-                     else {
-                         Toast toast = Toast.makeText(getApplicationContext(),"Tài khoản hoặc mật khẩu không chính xác.", Toast.LENGTH_SHORT);
-                         toast.show();
-
-                     }
-
-                 }
-             });
-    }
-    }
+//    package com.example.myapplication.user;
+//
+//    import androidx.appcompat.app.AppCompatActivity;
+//
+//    import android.annotation.SuppressLint;
+//    import android.content.Intent;
+//    import android.os.Bundle;
+//    import android.view.View;
+//    import android.widget.Button;
+//    import android.widget.EditText;
+//    import android.widget.Toast;
+//
+//    import com.example.myapplication.MainActivity;
+//    import com.example.myapplication.R;
+//
+//    public class Login extends AppCompatActivity {
+//        private Button btnLogin,btnSignup,login;
+//        private UserList userList;
+//        @SuppressLint({"WrongViewCast", "MissingInflatedId"})
+//        @Override
+//        protected void onCreate(Bundle savedInstanceState) {
+//            super.onCreate(savedInstanceState);
+//            setContentView(R.layout.activity_login);
+//
+//
+//            EditText loginvalue;
+//            EditText passw;
+//            loginvalue = findViewById(R.id.loginvalu);
+//            passw = findViewById(R.id.pas);
+//            btnLogin=(Button)findViewById(R.id.login);
+//            btnSignup=(Button)findViewById(R.id.btnSignup);
+//
+//            btnSignup.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    Intent intent = new Intent(Login.this,SignUp.class);
+//                    startActivity(intent);
+//                }
+//            });
+//
+//            login = findViewById(R.id.logi);
+//            login.setOnClickListener(new View.OnClickListener() {
+//                 @Override
+//                 public void onClick(View v) {
+//                        String name = loginvalue.getText().toString();
+//                        String pass = passw.getText().toString();
+//
+//                     Users user = new Users(name,pass);
+//                     if (user.checkValidEmail()&&user.checkValidPassword()){
+//                         if (user.checkValidUser()){
+//                             Toast toast = Toast.makeText(getApplicationContext(),"Đăng nhập thành công !", Toast.LENGTH_SHORT);
+//                             toast.show();
+//                             Intent intent2 = new Intent(Login.this, MainActivity.class);
+//                             startActivity(intent2);
+//                         }
+//                         else {
+//                             Toast toast = Toast.makeText(getApplicationContext(),"User not existed", Toast.LENGTH_SHORT);
+//                             toast.show();
+//                         }
+//                     }
+//                     else {
+//                         Toast toast = Toast.makeText(getApplicationContext(),"Tài khoản hoặc mật khẩu không chính xác.", Toast.LENGTH_SHORT);
+//                         toast.show();
+//
+//                     }
+//
+//                 }
+//             });
+//    }
+//    }
